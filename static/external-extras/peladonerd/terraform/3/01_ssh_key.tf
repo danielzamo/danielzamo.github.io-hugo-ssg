17dfc33f2d6e38272cbe07ffc5d0267c54aee33a
Daniel Oscar Zamo <daniel.oscar@gmail.com>
2023-06-26 03:18:41 +0200

    update name directory

--- static/external-extras/peladonerd/terraform/3/01_ssh_key.tf
+++
@@ -1,9 +0,0 @@
-#
-# Exportamos nuestra key SSH
-# Acordate de reemplazar el archivo id_rsa.pub con tu key de ssh publica
-
-resource "digitalocean_ssh_key" "key" {
-  name       = "key"
-  public_key = file("id_rsa.pub")
-}
-
--- static/external-extras/peladonerd/v2m/36/games/resources.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-kubectl get all,configmaps -l sparkfabrik.com/game=${1}
